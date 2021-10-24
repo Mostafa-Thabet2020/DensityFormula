235413ad42734d53defc4fe453ecc680c97eb527
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2021-10-24 22:32:13 +0200

    Add project files.

---
+++ DensityFormula/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DensityFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Density Formula");
+            Console.WriteLine("The density of material shows \n the denseness of it in a specific given area.");
+            // output
+            Console.WriteLine("Mass of the body: ");
+            //input
+            decimal m = Convert.ToDecimal(Console.ReadLine());
+
+            //output
+            Console.WriteLine("The volume of the body: ");
+            //input
+            decimal v = Convert.ToDecimal(Console.ReadLine());
+
+            //Equation
+            decimal p = m / v;
+            //result output 
+            Console.WriteLine("Density: "+p+" g/cm3");
+            
+            Console.ReadLine();
+        }
+    }
+}
